e8f963f4421afc8921782cd3a0d55de954fc8a12
Asli Sema Gultekin <aslisemagultekin@gmail.com>
2023-03-22 09:38:08 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+const divide = function(a, b) {
+    if(b !== 0){
+        return a/b
+    }
+    return "b can't be equal to 0"
+}
+
+const square = (number) => number * number
+
+const add = (num1, num2) => num1 + num2
